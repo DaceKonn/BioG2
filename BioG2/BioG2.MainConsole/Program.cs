46921013b9cca40d75b56005c7d5c66ce29412ab
DaceKonn <dacekonn@gmail.com>
2019-03-18 18:35:54 +0100

    Initial

---
+++ BioG2/BioG2.MainConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BioG2.MainConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
